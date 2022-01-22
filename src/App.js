029e0de8656cacaed3167d2b6b002188e43c8b90
bebusl <bebus1998@naver.com>
2022-01-23 07:50:00 +0900

    Feat: 페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Components from "./components/pages/Components";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route path="/components" component={Components}></Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
