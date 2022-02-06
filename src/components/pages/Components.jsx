29e5fbef7ca0ca1d84dc1eefce3691e7e425a11d
bebusl <bebus1998@naver.com>
2022-02-06 21:36:56 +0900

    Feat: all components add on Components page

--- src/components/pages/Components.jsx
+++ src/components/pages/Components.jsx
@@ -1,22 +1,28 @@
 import Modal from "../components/modal";
 import Loader from "../components/loader";
+import Texteditor from "../components/Texteditor";
+
 export default function Components() {
-    return (
+  return (
+    <div>
+      <h1>만든 컴포넌트 모음</h1>
+      <br />
+      <div>
+        <h2>텍스트에디터</h2>
+        <Texteditor></Texteditor>
+      </div>
+      <div>
+        <h2>모달</h2>
         <div>
-            <h1>만든 컴포넌트 모음</h1>
-            <br />
-            <div>
-                <h2>모달</h2>
-                <div>
-                    <Modal></Modal>
-                </div>
-            </div>
-            <div>
-                <h2>로딩바</h2>
-                <div>
-                    <Loader></Loader>
-                </div>
-            </div>
+          <Modal></Modal>
         </div>
-    );
+      </div>
+      <div>
+        <h2>로딩바</h2>
+        <div>
+          <Loader></Loader>
+        </div>
+      </div>
+    </div>
+  );
 }
